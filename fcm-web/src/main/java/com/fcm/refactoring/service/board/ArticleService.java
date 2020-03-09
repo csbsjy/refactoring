d4a8e59aa301574c3a4cedab456c4e9cb26cef6c
csbsjy <seojaeyeon@lotte.net>
2020-03-09 21:47:43 +0900

    refactoring-7 Article save service 구현 및 테스트작성 1. save 테스트는 Mocking 하려니까 뭔가 의미없는 기분 ,,? 더 알아봐야겠다. 2. 그래서 h2 이용하는 테스트를 하나 더 만들었다.

--- fcm-web/src/main/java/com/fcm/refactoring/service/board/ArticleService.java
+++ fcm-web/src/main/java/com/fcm/refactoring/service/board/ArticleService.java
@@ -4,7 +4,10 @@ package com.fcm.refactoring.service.board;
 import com.fcm.refactoring.board.domain.Article;
 import com.fcm.refactoring.board.dto.ArticleListResponseDto;
 import com.fcm.refactoring.board.dto.ArticleResponseDto;
+import com.fcm.refactoring.board.dto.ArticleUpdateRequestDto;
 import com.fcm.refactoring.board.repository.ArticleRepository;
+import com.fcm.refactoring.user.domain.User;
+import com.fcm.refactoring.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public List<ArticleListResponseDto> findAllList() {
@@ -26,6 +30,19 @@ public class ArticleService {
         Article findedArticle = articleRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException(String.format("id가 %d인 게시글이 없습니다.", id)));
         return new ArticleResponseDto(findedArticle);
+    }
+
+    public Long write(ArticleUpdateRequestDto articleUpdateRequestDto){
+        User user = userRepository.findByUserId(articleUpdateRequestDto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException(String.format("%s 아이디의 유저는 없습니다.", articleUpdateRequestDto.getUserId())));
+        Article article = Article.builder()
+                .user(user)
+                .subject(articleUpdateRequestDto.getSubject())
+                .build();
+
+        articleRepository.save(article);
+
+        return article.getId();
     }
 
 }
---
+++ fcm-web/src/test/java/com/fcm/refactoring/service/board/ArticleServiceRealTest.java
@@ -0,0 +1,39 @@
+package com.fcm.refactoring.service.board;
+
+import com.fcm.refactoring.board.domain.Article;
+import com.fcm.refactoring.board.dto.ArticleUpdateRequestDto;
+import com.fcm.refactoring.board.repository.ArticleRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@SpringBootTest
+@DisplayName("ArticleService Using h2 DB Test")
+public class ArticleServiceRealTest {
+
+    @Autowired
+    ArticleService articleService;
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @DisplayName("유저, 제목, 내용으로 게시글 정상저장 테스트")
+    @Test
+    void saveArticle() {
+
+        ArticleUpdateRequestDto articleUpdateRequestDto = new ArticleUpdateRequestDto("a1010100z@naver.com", "제목입니다", "내용입니다");
+
+        Long id = articleService.write(articleUpdateRequestDto);
+
+        Article article = articleRepository.findById(id).get();
+
+        assertAll(
+                () -> assertThat(article.getUser().getUserId()).isEqualTo("a1010100z@naver.com"),
+                () -> assertThat(article.getSubject()).isEqualTo("제목입니다")
+        );
+    }
+}
